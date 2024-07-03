4d8bb6eb022849f376473a06f083d6b185b2fa03
trenton-slocum <trent.slocum@hotmail.com>
2024-07-02 20:38:18 -0400

    Modified package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name = 'BookLover',
+    name = 'booklover',
     version = '1.0',
     description = "It's all about the books",
     url = 'https://github.com/trenton-slocum/BookLover',
